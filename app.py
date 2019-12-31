1409699504cafbaecb2bb18453f9107729652bb7
fulowa <fulowa@gmail.com>
2019-12-31 12:48:26 +0100

    adding chuck norris joke api

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import requests
 
-st.header('streamlit test app')
+
+st.header('Streamlit test app')
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
+
+
+
+st.header('chuck norris jokes')
+
+if st.button('tell me a chuck norris joke'):
+    response = requests.get("https://api.chucknorris.io/jokes/random")
+    st.write(response.json()['value'])
+
+
